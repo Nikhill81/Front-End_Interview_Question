be98583f5a5c320c1294084abcaac33f8e66c40c
Nikhill81 <156582384+Nikhill81@users.noreply.github.com>
2025-04-17 03:15:17 +0530

    Basic_revision

---
+++ Api/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("Search");
+const input = document.getElementById("city-name");
+
+const cityName = document.getElementById("country-name");
+const cityTime = document.getElementById("city-time");
+const cityTemp = document.getElementById("city-temp");
+
+async function getData(cityname) {
+  const promise =
+    await fetch(`http://api.weatherapi.com/v1/current.json?key=0cf6c445c3ef4f65891184114251604&q=${cityname}&aqi=yes
+ `);
+  return await promise.json();
+}
+
+button.addEventListener("click", async () => {
+  const value = input.value;
+  const result = await getData(value);
+  cityName.innerText = `${result.location.name},${result.location.region} - ${result.location.country}`;
+  cityTime.innerText = result.location.localtime;
+  cityTemp.innerText = result.current.temp_c;
+});
